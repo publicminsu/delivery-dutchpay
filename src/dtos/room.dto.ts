2397d70671ea0ed0d7b6cdbcfdbe7990f6f3db71
publicminsu <publicminsu@naver.com>
2021-11-16 14:52:21 +0900

    dto

--- src/dtos/room.dto.ts
+++ src/dtos/room.dto.ts
@@ -1,21 +1,30 @@
-import { Menu, TipInfo } from '@/interfaces/room.interface';
+import { Menu } from '@/entity/menu.entity';
+import { Tip } from '@/entity/tip.entity';
+import { IsEmail, IsEnum, IsNumber, IsString } from 'class-validator';
 
 export class CreateRoomDto {
+  @IsEmail({ allow_utf8_local_part: false })
   public userEmail: string;
-
+  @IsString()
   public shopName: string;
-  public tipInfos: TipInfo[];
-
+  @IsEnum(Tip)
+  public tipInfos: Tip[];
+  @IsEnum(Menu)
   public perchaserMenus: Menu[];
 }
 
 export class JoinRoomDto {
+  @IsNumber()
   public roomId: number;
+  @IsString()
   public userEmail: string;
 }
 
 export class AddMenuDto {
+  @IsNumber()
   public roomId: number;
+  @IsString()
   public userEmail: string;
+  @IsEnum(Menu)
   public menus: Menu[];
 }
